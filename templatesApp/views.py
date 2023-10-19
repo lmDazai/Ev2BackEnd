9c4d8236dff73ac446c5d98d4f64244b448fbf1e
Laboratorios Chillan <chilab@inacap.cl>
2023-10-19 08:53:54 -0300

    Guardado1

---
+++ templatesApp/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request,'templatesApp/index.html')
+
+
+def chile(request):
+    data={
+        "titulo": "Chile",
+        'producto1':"MAC",
+        'producto2':"Celular",
+        'producto3':"PlayStation",
+        'imagen':'imagenes/productos.jpg'
+    }
+    return render(request,'templatesApp/productos.html')
+
+def argentina(request):
+    data={
+        "titulo": "Argentina",
+        'producto1':"Pelota",
+        'producto2':"Figuera de Accion",
+        'producto3':"Juego de Mesa",
+        
+    }
+    return render(request,'templatesApp/productos.html')
+
+def peru(request):
+    data={
+        "titulo": "Peru",
+        'producto1':"POlera",
+        'producto2':"Chaqueta",
+        'producto3':"ZApatilla",
+        
+    }
+    return render(request,'templatesApp/productos.html')
+
+# Create your views here.
